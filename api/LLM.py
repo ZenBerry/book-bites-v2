3da70a6596e2ca1691715479d8e704dfa873d875
Eugene ZenBerry <eugeneberezhnoy@gmail.com>
2025-09-08 09:33:50 +0300

    New buttons

--- api/LLM.py
+++ api/LLM.py
@@ -3,9 +3,21 @@ from google import genai
 client = genai.Client(api_key="AIzaSyCO4CaWfuolgFco93vdGbYEjqOf9CcqXws")
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Please list 5 books similar to Moral Letters to Lucilius. Plain list of 5 names, nothing more, please",
+    model="gemma-3-27b-it",
+    contents="Please list 5 authors similar to Tony Robbins. Plain list of 5 names, nothing more, please",
 
 )
 
 print(response.text)
+
+# print("List of models that support generateContent:\n")
+# for m in client.models.list():
+#     for action in m.supported_actions:
+#         if action == "generateContent":
+#             print(m.name)
+
+# print("List of models that support embedContent:\n")
+# for m in client.models.list():
+#     for action in m.supported_actions:
+#         if action == "embedContent":
+#             print(m.name)
